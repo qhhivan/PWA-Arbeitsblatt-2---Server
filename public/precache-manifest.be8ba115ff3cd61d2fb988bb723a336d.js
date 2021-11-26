1193800bec66f11806ec0563f65648d8e442ebb6
qhhivan <orsolic.i02@htlwienwest.at>
2021-11-26 14:10:15 +0100

    Fertig

---
+++ public/precache-manifest.be8ba115ff3cd61d2fb988bb723a336d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf7a1c7c566518425b6",
+    "url": "/css/app.112888a1.css"
+  },
+  {
+    "revision": "a90f96cefdc4a630f588",
+    "url": "/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "31287f1a79c128368c41fa51e5f6f045",
+    "url": "/images/portraits/men/20.jpg"
+  },
+  {
+    "revision": "561f7971c5fdf22351c11db6a2a07a79",
+    "url": "/images/portraits/men/22.jpg"
+  },
+  {
+    "revision": "c7c6787453721d8c646b00a469136eb3",
+    "url": "/images/portraits/men/27.jpg"
+  },
+  {
+    "revision": "182ac2d837a585ade84e5f2ac3dff416",
+    "url": "/images/portraits/men/37.jpg"
+  },
+  {
+    "revision": "0f4246ee8b6dd185af6607d249a29efe",
+    "url": "/images/portraits/men/41.jpg"
+  },
+  {
+    "revision": "ab82a70c4d7efc7199176a584c660a7d",
+    "url": "/images/portraits/men/59.jpg"
+  },
+  {
+    "revision": "bad571820f3bcf60ed96c4355dbbebfd",
+    "url": "/images/portraits/women/0.jpg"
+  },
+  {
+    "revision": "45b194eeab62e029b4ac5d4d4b1b3424",
+    "url": "/images/portraits/women/26.jpg"
+  },
+  {
+    "revision": "2f8d87bab3c46025b9d534fb2d9e3fef",
+    "url": "/images/portraits/women/36.jpg"
+  },
+  {
+    "revision": "bf8fa1e2fc2c45942e460aa132a8fa51",
+    "url": "/images/portraits/women/37.jpg"
+  },
+  {
+    "revision": "76e496fa87e94ce407626ed44f837bac",
+    "url": "/images/portraits/women/46.jpg"
+  },
+  {
+    "revision": "6c721e4fad295a44eb98441a192010ae",
+    "url": "/images/portraits/women/5.jpg"
+  },
+  {
+    "revision": "47ab23b443cea564d765c5e015344420",
+    "url": "/images/portraits/women/52.jpg"
+  },
+  {
+    "revision": "b1566bee0ab3959050f638b8c3079c50",
+    "url": "/images/portraits/women/59.jpg"
+  },
+  {
+    "revision": "95d9cae6815044f79b28f8fcffe121c4",
+    "url": "/images/portraits/women/61.jpg"
+  },
+  {
+    "revision": "e26678e2b484d8d1c3b5caefbcd2b7ba",
+    "url": "/images/portraits/women/70.jpg"
+  },
+  {
+    "revision": "cfd612df09b152ac9f0de74f3034e554",
+    "url": "/images/portraits/women/71.jpg"
+  },
+  {
+    "revision": "343eaffcfb2fa887e86b13ae5a5e1979",
+    "url": "/images/portraits/women/72.jpg"
+  },
+  {
+    "revision": "5e91b89e1853920bb0069e48726f4f7d",
+    "url": "/images/portraits/women/88.jpg"
+  },
+  {
+    "revision": "8b1d98e061f0a44d6d5676c33cb55755",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdf7a1c7c566518425b6",
+    "url": "/js/app.93bfc102.js"
+  },
+  {
+    "revision": "a90f96cefdc4a630f588",
+    "url": "/js/chunk-vendors.15946ed5.js"
+  },
+  {
+    "revision": "f5f4e3b7714f27fc71f2b674ede66cd1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
